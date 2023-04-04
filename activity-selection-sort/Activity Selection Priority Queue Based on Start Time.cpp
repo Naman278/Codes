270ee8ad2c216d208c71845efdc25772df485c18
Naman278 <108816021+Naman278@users.noreply.github.com>
2023-04-05 01:41:44 +0530

    Create Activity Selection Priority Queue Based on Start Time.cpp

---
+++ activity-selection-sort/Activity Selection Priority Queue Based on Start Time.cpp
@@ -0,0 +1,14 @@
+/*
+Given a set ‘S’ of ‘n’ activities, implement the recursive greedy algorithm to select a subset of activities from S by selecting the task that starts last. Build a priority queue (max heap) based on start time and then select the activities
+
+Input Format
+
+First line contains the number of activities, n
+
+Next 'n' line contains the details of the activities such as name of activity, start time and finish time
+
+Output Format
+
+Print the name of the activities that are selected separated by a space
+*/
+
